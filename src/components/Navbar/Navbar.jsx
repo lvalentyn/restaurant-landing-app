db4132508289444c0ea3b027213c04a51a903564
Ewig <valikallstars@gmail.com>
2022-03-27 18:07:32 +0300

    added container: aboutUS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const navList = ['home', 'about', 'menu', 'awards', 'contacts'];
 
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -15,11 +16,9 @@ const Navbar = () => {
         <img src={images.gericht} alt="app-logo" />
       </div>
       <ul className="app__navbar-links">
-        <li className="p__opensans"><a href="#home">Home</a></li>
-        <li className="p__opensans"><a href="#about">About</a></li>
-        <li className="p__opensans"><a href="#menu">Menu</a></li>
-        <li className="p__opensans"><a href="#awards">Awards</a></li>
-        <li className="p__opensans"><a href="#contacts">Contacts</a></li>
+        {navList.map((el, i) => (
+          <li key={el + i} className="p__opensans"><a href={`#${el}`}>{el}</a></li>
+        ))}
       </ul>
       <div className="app__navbar-login">
         <a href="#login" className='p__opensans'>Log In / Register</a>
@@ -34,11 +33,9 @@ const Navbar = () => {
             <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
               <MdOutlineRestaurantMenu className='overlay__close' fontSize={27} onClick={() => { setToggleMenu(false) }} />
               <ul className="app__navbar-smallscreen-links">
-                <li className="p__opensans"><a href="#home">Home</a></li>
-                <li className="p__opensans"><a href="#about">About</a></li>
-                <li className="p__opensans"><a href="#menu">Menu</a></li>
-                <li className="p__opensans"><a href="#awards">Awards</a></li>
-                <li className="p__opensans"><a href="#contacts">Contacts</a></li>
+                {navList.map((el, i) => (
+                  <li key={el + i} className="p__opensans" onClick={() => { setToggleMenu(false) }}><a href={`#${el}`}>{el}</a></li>
+                ))}
               </ul>
             </div>
           )}
