a4747b677314d250186ba948a39075fa323df198
Ewig <valikallstars@gmail.com>
2022-03-27 21:33:34 +0300

    added container: 'Laurels'

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.scss';
 
 const Laurels = () => (
-  <div>
-    Laurels
-  </div>
+	<div className='app__bg app__wrapper section__padding' id='awards'>
+		<div className="app__wrapper_info">
+			<SubHeading title='Awards & recognition' />
+			<h2 className='headtext__cormorant'>Our Laurels</h2>
+
+			<div className="app__laurels_awards">
+				{data.awards.map((award, i) => (
+					<AwardCard award={award} key={award.title + i} />
+				))}
+			</div>
+		</div>
+		<div className="app__wrapper_img">
+			<img src={images.laurels} alt="laurels" />
+		</div>
+
+	</div>
 );
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
+	return (
+		<div className="app__laurels_awards-card">
+			<img src={imgUrl} alt={title} />
+			<div className="app__laurels_awards-card_content">
+				<p className="p__cormorant" style={{ color: '#DCCA87' }}>{subtitle}</p>
+				<p className="p__cormorant">{title}</p>
+			</div>
+		</div>
+	);
+}
 
 export default Laurels;
